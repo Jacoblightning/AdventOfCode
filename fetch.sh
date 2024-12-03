7a42c629148f63f7e730d64fa1a0721d5205e91b
Jacob Freeman <t00niyf5@duck.com>
2024-12-03 13:57:25 -0500

    Add warning

--- fetch.sh
+++ fetch.sh
@@ -3,5 +3,9 @@
 
 set -e
 
+# User has control of request rate.
+echo "As per automation rules, please don't repeatedly run this script"
 wget -O "$1/day$2/input.txt" -U "https://github.com/jacoblightning/AdventOfCode/master/fetch.sh Jacob Freeman (jacoblightning3.aternos.me@gmail.com)" "https://adventofcode.com/$1/day/$2/input"
 cat "$1/day$2/input.txt"
+echo
+echo "As per automation rules, please don't repeatedly run this script"
