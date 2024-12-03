05defc687e9e0d581c06628a6b19a62eaf1b0a71
Jacob Freeman <t00niyf5@duck.com>
2024-12-03 18:08:03 -0500

    Add cookies.

--- fetch.sh
+++ fetch.sh
@@ -8,8 +8,11 @@ python -c "import webbrowser;webbrowser.open('https://adventofcode.com/$1/day/$2
 
 # User has control of request rate.
 echo "As per automation rules, please don't repeatedly run this script"
-wget -O "$1/day$2/input.txt" -U "https://github.com/jacoblightning/AdventOfCode/master/fetch.sh Jacob Freeman (jacoblightning3.aternos.me@gmail.com)" "https://adventofcode.com/$1/day/$2/input"
+wget -U "https://github.com/Jacoblightning/AdventOfCode/blob/master/fetch.sh Jacob Freeman (jacoblightning3.aternos.me@gmail.com)" \
+  --load-cookies cookies.txt \
+  --compression=auto \
+  -O "$1/day$2/input.txt"  \
+  "https://adventofcode.com/$1/day/$2/input"
 cat "$1/day$2/input.txt"
 echo
 echo "As per automation rules, please don't repeatedly run this script"
-python
