2f4da9b5cd8ee24a6dd670ef334cddbf0a8142f8
Jacob Freeman <t00niyf5@duck.com>
2024-12-03 14:17:05 -0500

    Make code cleaner

--- 2024/day2/solver.py
+++ 2024/day2/solver.py
@@ -3,28 +3,19 @@ with open("input.txt") as fd:
 
 data = [i.split() for i in puzzle]
 
-# Bad code.
-#TODO: Clean this up
 def linesafe(line):
     # Right, we can sort
     if not (line == sorted(line) or line == sorted(line, reverse=True)):
         return False
 
-    thissafe = True
-    last = False
-    second = False
-    for i in line:
-        if last is False:
-            last = i
-            # The next one will be the second number in the sequence
-            second = True
-            continue
-        diff = abs(int(last) - int(i))
+    for cnt, i in enumerate(line):
+        if cnt == 1: continue
+
+        diff = abs(int(line[cnt-1]) - int(i))
+
         if not 0 < diff < 4:
-            thissafe = False
-            break
-        last = i
-    return thissafe
+            return False
+    return True
 
 def solve1():
     safe = 0
